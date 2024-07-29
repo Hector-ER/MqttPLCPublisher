5bf01b3452641cc3036a2a8e0f7cfc126fbfadde
Héctor <hectorerey@yahoo.com>
2024-07-28 21:05:48 -0300

    Se agrego Usuario y Password al Broker

--- MqttPLCPublisher/Broker.cs
+++ MqttPLCPublisher/Broker.cs
@@ -14,6 +14,8 @@ namespace MqttPLCPublisher
         public String Nombre = "";
         public String Direccion = "";
         public int Puerto = 0;
+        public String Usuario = "";
+        public String Password = "";
 
         public IMqttClient? mqttClient;
 
@@ -21,7 +23,36 @@ namespace MqttPLCPublisher
 
         public Broker(XmlNode conf)
         {
-            if (conf.Attributes.GetNamedItem("Name") != null)
+            if (conf.Attributes != null)
+            {
+                foreach (XmlAttribute a in conf.Attributes)
+                {
+                    if ("NAME".CompareTo(a.Name.ToUpper()) == 0)
+                    {
+                        Nombre = a.Value.ToUpper();
+                    }
+                    else if ("ADDRESS".CompareTo(a.Name.ToUpper()) == 0)
+                    {
+                        Direccion = a.Value.ToUpper();
+                    }
+                    else if ("PORT".CompareTo(a.Name.ToUpper()) == 0)
+                    {
+                        Puerto = Int32.Parse(a.Value);
+                    }
+                    else if ("USER".CompareTo(a.Name.ToUpper()) == 0)
+                    {
+                        Usuario = a.Value.ToUpper();
+                    }
+                    else if ("PASSWORD".CompareTo(a.Name.ToUpper()) == 0)
+                    {
+                        Password = a.Value.ToUpper();
+                    }
+
+                }
+            }
+
+            /*
+             if (conf.Attributes.GetNamedItem("Name") != null)
             {
                 Nombre = conf.Attributes.GetNamedItem("Name").Value;
             }
@@ -32,7 +63,7 @@ namespace MqttPLCPublisher
             if (conf.Attributes.GetNamedItem("Port") != null)
             {
                 Puerto = Int32.Parse(conf.Attributes.GetNamedItem("Port").Value);
-            }
+            }*/
         }
 
         public async void conectar()
@@ -41,8 +72,8 @@ namespace MqttPLCPublisher
             int port = Puerto; // 1883;
             string clientId = Guid.NewGuid().ToString();
             //string topic = "Csharp/mqtt";
-            //string username = "emqxtest";
-            //string password = "******";
+            string username = Usuario; // "emqxtest";
+            string password = Password; // "******";
 
             // Create a MQTT client factory
             factory = new MqttFactory();
@@ -53,13 +84,18 @@ namespace MqttPLCPublisher
             // Create MQTT client options
             var options = new MqttClientOptionsBuilder()
                 .WithTcpServer(broker, port) // MQTT broker address and port
-                                             //.WithCredentials(username, password) // Set username and password
+                .WithCredentials(username, password) // Set username and password
                 .WithClientId(clientId)
                 .WithCleanSession()
                 .Build();
-            var connectResult = mqttClient.ConnectAsync(options);
-
-            System.Threading.Thread.Sleep(10000);
+            //var connectResult = await mqttClient.ConnectAsync(options);
+            var t = mqttClient.ConnectAsync(options);
+            DateTime ti = DateTime.Now;
+            while ((DateTime.Now - ti).TotalSeconds < 50) 
+            {
+                if (mqttClient.IsConnected) { break; }
+            }
+            //System.Threading.Thread.Sleep(10000);
 
         }
     }
